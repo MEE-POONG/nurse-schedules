208dcb73b892975a10cc95d1385508231748f19b
Pattawut Biakrathok <62004847+bens0746@users.noreply.github.com>
2022-12-27 17:57:05 +0700

    add Month and Year Select

--- components/TableIndex/TableIndex.jsx
+++ components/TableIndex/TableIndex.jsx
@@ -1,11 +1,24 @@
 import { TableCurrentMonth } from "@/components/TableCurrentMonth/TableCurrentMonth";
 import React from "react";
+import DropDownDate from "../DropDownDate/DropDownDate";
+import { TableSelectMonth } from "../TableSelectMonth/TableSelectMonth";
+
+const inputM = 4
+const inputY = 2023
 
 export default function TableIndex() {
 
   return (
     <>
-      <TableCurrentMonth />
+      <div className="text-center mt-6">
+        <DropDownDate/>
+      </div>
+      {
+        inputM === ''  && inputY === '' 
+        ?<TableCurrentMonth/>
+        :<TableSelectMonth
+        />
+      }
     </>
   );
 }
