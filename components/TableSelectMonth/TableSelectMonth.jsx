8dcaee1f53ee8575dc5c41af011e9a35661b7a9f
ChunWarayut <warayut2555@gmail.com>
2023-02-15 02:10:19 +0700

    .format("MMMM")

--- components/TableSelectMonth/TableSelectMonth.jsx
+++ components/TableSelectMonth/TableSelectMonth.jsx
@@ -131,7 +131,7 @@ export const TableSelectMonth = ({
               <tr className="bg-white">
                 <td className="border border-white" colSpan={daysInMonth + 9}>
                   <div className="text-center text-lg">
-                    ตารางเวรประจำเดือน.........................{monthTH}
+                    ตารางเวรประจำเดือน.........................{dayjs(+monthValue + 1).format("MMMM")}
                     .........................พ.ศ...............{yearTH}
                     ................
                   </div>
