18372f1d5b43ea63ccd0605e0e41831d5a60e060
ChunWarayut <warayut2555@gmail.com>
2023-05-17 22:18:02 +0700

    check id

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,22 @@
 import TableIndex from "@/components/TableIndex/TableIndex";
 import dayjs from "dayjs";
+import { useEffect } from "react";
+import { authProvider } from "src/authProvider";
 
 export default function Home() {
+  useEffect(() => {
+    if (authProvider.getIdentity().id === undefined) {
+      authProvider.logout();
+      window.location.href = "/login";
+    }
+  })
   return (
     <>
-    <div className="my-5">
-      <div className="text-center mt-10">
-        <h1>สรุปยอดตารางเวรประจำเดือน</h1>
+      <div className="my-5">
+        <div className="text-center mt-10">
+          <h1>สรุปยอดตารางเวรประจำเดือน</h1>
+        </div>
       </div>
-    </div>
       <TableIndex />
     </>
   );
