70d8f747e90eda30428e768023d984265601497b
warayut teakrathok <warayut2555@gmail.com>
2024-09-30 21:43:47 +0700

    up font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["20px", "28px"],
+      xl: ["24px", "32px"],
+      "2xl": ["28px", "36px"],
+      "3xl": ["32px", "40px"],
+      "4xl": ["36px", "44px"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
